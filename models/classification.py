b314e9c5d367938c7ecdef87105ac0953b47274a
Anandhakrishnan <anandhakrishnanm21@gmail.com>
2026-03-31 18:20:56 +0530

    classification

--- models/classification.py
+++ models/classification.py
@@ -4,6 +4,9 @@
 import torch
 import torch.nn as nn
 
+from layers import CustomDropout
+from vgg11 import VGG11Encoder
+
 
 class VGG11Classifier(nn.Module):
     """Full classifier = VGG11Encoder + ClassificationHead."""
@@ -16,7 +19,19 @@ class VGG11Classifier(nn.Module):
             in_channels: Number of input channels.
             dropout_p: Dropout probability for the classifier head.
         """
-        pass
+        super().__init__()
+        self.encoder = VGG11Encoder(in_channels=in_channels)
+        self.avgpool = nn.AdaptiveAvgPool2d((7, 7)) # Global average pooling to reduce spatial dimensions
+        self.classifier = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(512 * 7 * 7, 4096),
+            nn.ReLU(inplace=True),
+            CustomDropout(p=dropout_p),
+            nn.Linear(4096, 4096),
+            nn.ReLU(inplace=True),
+            CustomDropout(p=dropout_p),
+            nn.Linear(4096, num_classes),
+        )
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         """Forward pass for classification model.
@@ -25,5 +40,13 @@ class VGG11Classifier(nn.Module):
         Returns:
             Classification logits [B, num_classes].
         """
-        # TODO: Implement forward pass.
-        raise NotImplementedError("Implement VGG11Classifier.forward")
+        feats = self.encoder(x)
+        feats = self.avgpool(feats)
+        return self.classifier(feats)
+    
+if __name__ == "__main__":
+    model = VGG11Classifier(num_classes=37, in_channels=3, dropout_p=0.5)
+    input_tensor = torch.randn(1, 3, 224, 224)  # Example input
+    output = model(input_tensor)
+    print("Output shape:", output.shape)
+    print("Output logits:", output)
